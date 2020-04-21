89c78758ccd91991338cd265e74dd91861d3800f
Jonathan Rosser <jonathan.rosser@rd.bbc.co.uk>
2020-04-21 07:34:18 +0100

    Convert fuzzy_bidirectional_search filter to python3

--- filter_plugins/fuzzy_bidirectional_search.py
+++ filter_plugins/fuzzy_bidirectional_search.py
@@ -1,13 +1,15 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import re
 
 # Filter to check a string against a list for matches
 def fuzzy_bidirectional_search(check_list, check_string):
     # Check for any string matches on the list item, or vice versa
     for list_item in check_list:
-        if re.search(check_string,list_item):
+        check_string_escaped = re.escape(check_string)
+        list_item_escaped = re.escape(list_item)
+        if re.search(check_string_escaped,list_item):
             return True
-        if re.search(list_item,check_string):
+        if re.search(list_item_escaped,check_string):
             return True
     return False
 
